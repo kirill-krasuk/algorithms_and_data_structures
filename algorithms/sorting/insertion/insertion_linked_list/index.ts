aed3ecaa7c183157b22f0f35c76dc86d4a9ec33e
kirill-krasuk <naviinsane@gmail.com>
2023-08-09 14:40:23 +0300

    add test and refactor insertion sort over list

--- algorithms/sorting/insertion/insertion_linked_list/index.ts
+++ algorithms/sorting/insertion/insertion_linked_list/index.ts
@@ -1,29 +1,49 @@
 import LinkedNode from '../../../../data_structures/list/linked-list/Node';
 
+function findInsertionPoint(
+	dummy: LinkedNode<number>,
+	current: LinkedNode<number>,
+): LinkedNode<number> {
+	// preInsert.next is the insertion point
+	let preInsert = dummy;
+
+	// find the insertion point in the sorted list
+	while (
+		preInsert.next
+		&& preInsert.next !== current
+		&& preInsert.next.value <= current.value
+	) {
+		preInsert = preInsert.next;
+	}
+
+	return preInsert;
+}
+
 function insertionSortList(head: LinkedNode<number> | null): LinkedNode<number> | null {
-	const dummy = new LinkedNode(0, head);
+	if (!head || !head.next) return head;
 
+	const dummy = new LinkedNode(0);
 	let preCurrent = dummy;
-	let current = dummy.next;
 
-	while (current) {
-		let preInsert = dummy;
-		let currentInsert = dummy.next!;
+	dummy.next = head;
 
-		while (currentInsert !== current && currentInsert.value <= current.value) {
-			preInsert = currentInsert;
-			currentInsert = currentInsert.next!;
-		}
+	// preCurrent.next is the current node
+	while (preCurrent.next) {
+		const preInsert = findInsertionPoint(dummy, preCurrent.next);
 
-		if (currentInsert !== current) {
-			preCurrent.next = current.next;
-			current.next = preInsert.next;
-			preInsert.next = current;
+		// if preInsert.next is not equal to preCurrent.next
+		// then we need to insert preCurrent.next to the sorted list
+		if (preInsert.next !== preCurrent.next) {
+			const temp = preCurrent.next!;
 
-			current = preCurrent.next;
+			preCurrent.next = temp.next;
+			temp.next = preInsert.next;
+			preInsert.next = temp;
 		} else {
-			preCurrent = current;
-			current = current.next;
+			// if preInsert.next is equal to preCurrent.next then
+			// preCurrent.next is already in the sorted list
+			// so we just move preCurrent to the next node
+			preCurrent = preCurrent.next!;
 		}
 	}
 
